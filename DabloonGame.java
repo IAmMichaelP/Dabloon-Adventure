96a8f7c2846271714ae59903eabfde4759870d38
catcatg <121284621+catcatg@users.noreply.github.com>
2022-12-25 00:06:31 +0800

    Add files via upload

--- DabloonGame.java
+++ DabloonGame.java
@@ -1,148 +1,153 @@
 package project;
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class DabloonGame extends Encounters {
 	private static Scanner scan = new Scanner(System.in);
-	ArrayList<String> list = new ArrayList<String>();
-	protected int dabloonWallet;
-	private int event;
+	private int dabloonWallet;
+	private Map<String, Integer> storeInventory;
+	private Map<String, Integer> userInventory;
+	public DabloonGame () {
+	    // Create the store inventory
+	    storeInventory = new HashMap<>();
+	    storeInventory.put("water", 10);
+	    storeInventory.put("map", 15);
+	    storeInventory.put("torch", 40);
+	    storeInventory.put("blanket", 80);
+	    storeInventory.put("tent", 200);
+	    // Create the user's inventory
+	    userInventory = new HashMap<>();
+	    scan = new Scanner(System.in);
+	  }
+	
+	public class Game{
+		Merchant merchant = new Merchant();
+		public void play(){
+			Traveller traveller = new Traveller();
+			
+			System.out.println("\n########################################################################################"
+					+ "\nHello Traveller! I see you're new to this world.\nMay I know your name?.\nEnter your name: ");
 
-	public DabloonGame(){
+			String name = scan.nextLine();
+			Traveller.setName(name);
 
-		Traveller traveller = new Traveller();
-
-		System.out.println("\n########################################################################################"
-				+ "\nHello Traveller! I see you're new to this world.\nMay I know your name?.\nEnter your name: ");
-
-		String name = scan.nextLine();
-		Traveller.setName(name);
-
-		System.out.println("\n########################################################################################");
-		System.out.println("\nHello, " + Traveller.getName() + ".");
-		setDabloons(traveller.RandomDabloons(dabloonWallet));
-		dabloonWallet = getDabloons();
-
-		encounters(dabloonWallet);
-
-	}
-
-
-
-	public void encounters(int dabloonWallet){
-
-		Boolean isLoop = true;
-
-		while(dabloonWallet < 2000 && isLoop == true) {
 			System.out.println("\n########################################################################################");
-			System.out.println("\nDo you wish to travel?");
-			System.out.println("\nYes / No");
-			System.out.println("Enter 'Inventory' to check inventory. ");
-			String ans = scan.nextLine();
-			String choice = ans.toLowerCase();
+			System.out.println("\nHello, " + Traveller.getName() + ".");
+			setDabloons(traveller.RandomDabloons(dabloonWallet));
+			dabloonWallet = getDabloons();
 
-			if(ans.equalsIgnoreCase("no")) {
-				isLoop = false;
-			}
-			else {
+			encounters(dabloonWallet);
+			
+		}
+		
+		public void encounters(int dabloonWallet){
 
-				switch(choice) {
-				case "yes":{
 
-					Random rEvent = new Random(); 
-					event = rEvent.nextInt(1,3); 
+			Boolean isLoop = true;
 
-					switch(event){
-					case 1: {
-						dabloonWallet = task(dabloonWallet);
-						continue;
-					}
-					case 2: {
-						if(dabloonWallet > 200) {
-							dabloonWallet = merchant(dabloonWallet, itemNum);
-							continue;
-						}else {
-							event = 1;
+
+			while(dabloonWallet < 2000 && isLoop == true) {
+				System.out.println("\n########################################################################################"
+						+ "\nPRESS:\n[1]TRAVEL\n[2]REST\n[3]INVENTORY");
+				String action = scan.nextLine();
+				int parsedAnswer = answerParser(action);
+
+				if(parsedAnswer == 2) {
+					isLoop = false;
+				}
+				else {
+					System.out.println("\nparsed answer is"+parsedAnswer+"\n");
+					if(parsedAnswer ==1) {
+						System.out.println("Caught inside switch");
+					
+						int randomEvent = randomizer(dabloonWallet); 
+						
+						switch(randomEvent){
+						case 1: {
+							dabloonWallet = task(dabloonWallet);
+							System.out.println("\nJust exited task heres how much we've earned: " + dabloonWallet);
 							continue;
 						}
+						case 2: {
+							
 
-					}
-					case 3: {
+							dabloonWallet = merchant.merchant(dabloonWallet);
+							
+						}
+						case 3: {//thieves
 
-						Random stolen = new Random(); 
-						int stolenDabloons = stolen.nextInt(50);
-						if(dabloonWallet >= 500) {
-
+							Random stolen = new Random(); 
+							int stolenDabloons = stolen.nextInt(10,50);
+					
 							System.out.println("\nOh no! There are thieves!!");
 							System.out.println("Dabloons is decreased by: " + stolenDabloons);
 							dabloonWallet -= stolenDabloons;
 							System.out.println(dabloonWallet + " Dabloons left after encountering thieves.");
-							continue;
-						}else {
-							event = 1;
-							continue;
+								
+						
+							}
+						default:{
+							System.out.println("caught in the inner default");
+						}
+
 						}
 
 					}
-					default: continue;
+					else if (parsedAnswer == 3){
+						
+						System.out.println("\nYour inventory: ");
+					    for (String item : userInventory.keySet()) {
+					    	System.out.println(item + userInventory.get(item));
+					    	
+					    }
+						continue;
 					}
-
-				}
-				case "no":{
-					isLoop = false;
-				}
-				case "inventory" :{
-					info();
-					continue;
-				}
-				default:continue;
+					else{
+						System.out.println("You receive a default");
+						continue;
+					}
 				}
 
+				}
+				
 			}
-		}
 
+		}
+	
+
+	@Override
+	public int answerParser (String action) {
+		System.out.println("you catch in answerParser");
+		try {
+			  int num = Integer.parseInt(action);
+			  return num;
+			} catch (NumberFormatException e) {
+			  System.out.println("Invalid number format");
+			  return 4;
+			}
 	}
 
-
-	protected void info(){
-
-		switch(itemNum) {
-
-		case 1 :{
-			list.add("Water ");
-
-			break;
+	public int randomizer(int dabloonWallet) {
+		System.out.println("Caught in the randomizer");
+		Random rEvent = new Random(); 
+		int randomEvent = rEvent.nextInt(3)+1;
+		System.out.println("randomEvent is "+randomEvent +"\ndabloonWallet is "+dabloonWallet);
+		if((dabloonWallet < 200 && randomEvent == 2) || (dabloonWallet < 500 && randomEvent == 3)) {
+			randomEvent = 1;
+			System.out.println("randomEvent is "+randomEvent +"\ndabloonWallet is "+dabloonWallet);
+			return randomEvent;
+		} else {
+			System.out.println("randomEvent is "+randomEvent +"\ndabloonWallet is "+dabloonWallet);
+			return randomEvent;
 		}
-		case 2:{
-			list.add("Map ");
-
-			break;
-		}
-		case 3:{
-			list.add("Torch ");
-
-			break;
-		}
-		case 4:{
-			list.add("Blanket ");
-
-			break;
-		}
-		case 5:{
-			list.add("Tent ");
-
-			break;
-		}
-		default:break;
-		}
-		System.out.println("Traveller name: " + name + "\nDabloons: " + dabloonWallet);
-		System.out.println("Items in Inventory: " + list);
+		
+		
+		
 	}
-
-
-
+	
 	public int getDabloons(){
 		return dabloonWallet;
 	}
@@ -151,134 +156,140 @@ public class DabloonGame extends Encounters {
 		this.dabloonWallet = dabloonWallet;
 	}
 
+	public class Merchant {
+		public int merchantAnswerParser (String answer) {
+			try {
+				  int num = Integer.parseInt(answer);
+				  return num;
+				} catch (NumberFormatException e) {
+				  System.out.println("Invalid number format");
+				  return 6;
+				}
+		}
+		public int merchant(int dabloonWallet) {
+		    
+
+			System.out.println("\n########################################################################################");
+			System.out.println("Encountered a shop along the way..");
+			Boolean buy = true;
 
 
+			while(buy == true) {
+				System.out.println("\nWELCOME TO THE SHOP");
+				System.out.println("Attendant: What item would you like to buy? We have a few...");
+				System.out.println("\nStore inventory: ");
+			    for (String item : storeInventory.keySet()) {
+			      System.out.println(item + " - $" + storeInventory.get(item));
+			    }
+				System.out.println("\nWallet: " + dabloonWallet
+						+"\nEnter designated number to buy the item, any other key to leave." + "\n[1]Water  [2]Map  [3]Torch   [4]Blanket  [5]Tent [0]Exit shop");
+			    System.out.println("\nYour inventory: ");
+			    for (String item : userInventory.keySet()) {
+			      System.out.println(item + userInventory.get(item));
+			    }
+				String answer = scan.nextLine();
+				int parsedAnswer = merchantAnswerParser(answer);
+
+				if (dabloonWallet < 10) {
+					System.out.println("You don't have enough amount of dabloons left to buy anything");
+					continue;
+				}
+				else if(parsedAnswer == 1 && storeInventory.containsKey("water")) {
+					System.out.println("\nWater is added to inventory.");
+					// Add the item to the user's inventory
+			        int quantity = userInventory.getOrDefault("water", 0);
+			        userInventory.put("Water", quantity + 1);
+
+			        // Remove the item from the store inventory
+			        quantity = storeInventory.get("water");
+			        if (quantity > 1) {
+			          storeInventory.put("water", quantity - 1);
+			        } else {
+			          storeInventory.remove("water");
+			        }
+					dabloonWallet -= 10;
+				}
+				else if(parsedAnswer == 2 && storeInventory.containsKey("map")) {
+					System.out.println("\nMap is added to inventory.");
+					// Add the item to the user's inventory
+			        int quantity = userInventory.getOrDefault("map", 0);
+			        userInventory.put("map", quantity + 1);
+
+			        // Remove the item from the store inventory
+			        quantity = storeInventory.get("map");
+			        if (quantity > 1) {
+			          storeInventory.put("map", quantity - 1);
+			        } else {
+			          storeInventory.remove("map");
+			        }
+					dabloonWallet -= 15;
+				}
+				else if(parsedAnswer == 3 && storeInventory.containsKey("torch")) {
+					System.out.println("\nTorch is added to inventory.");
+					// Add the item to the user's inventory
+			        int quantity = userInventory.getOrDefault("torch", 0);
+			        userInventory.put("torch", quantity + 1);
+
+			        // Remove the item from the store inventory
+			        quantity = storeInventory.get("torch");
+			        if (quantity > 1) {
+			          storeInventory.put("torch", quantity - 1);
+			        } else {
+			          storeInventory.remove("torch");
+			        }
+					dabloonWallet -= 40;
+				}
+				else if(parsedAnswer == 4 && storeInventory.containsKey("blanket")) {
+					System.out.println("\nBlanket is added to inventory.");
+					// Add the item to the user's inventory
+			        int quantity = userInventory.getOrDefault("blanket", 0);
+			        userInventory.put("blanket", quantity + 1);
+
+			        // Remove the item from the store inventory
+			        quantity = storeInventory.get("blanket");
+			        if (quantity > 1) {
+			          storeInventory.put("blanket", quantity - 1);
+			        } else {
+			          storeInventory.remove("blanket");
+			        }
+					dabloonWallet -= 80;
+				}
+				else if(parsedAnswer == 5 && storeInventory.containsKey("tent")) {
+					System.out.println("\nTent is added to inventory.");
+					// Add the item to the user's inventory
+			        int quantity = userInventory.getOrDefault("tent", 0);
+			        userInventory.put("tent", quantity + 1);
+
+			        // Remove the item from the store inventory
+			        quantity = storeInventory.get("tent");
+			        if (quantity > 1) {
+			          storeInventory.put("tent", quantity - 1);
+			        } else {
+			          storeInventory.remove("tent");
+			        }
+					dabloonWallet -= 200;
+				}
+				else {
+					System.out.println("You left the shop.");
+					buy = false;
+				}
+			}return dabloonWallet;
+		}
+	}
 	public static void main(String[] args){
-		DabloonGame game = new DabloonGame();
+		
 
-		System.out.println("\nDabloons earned after playing: " + game.dabloonWallet);
+
+		
+		DabloonGame dabloonGame = new DabloonGame();
+		DabloonGame.Game game = dabloonGame.new Game();
+		game.play();
+		
+		System.out.println("\nDabloons earned after playing: " );
 		System.out.println("Thank you for playing!");
 		System.out.println("Closing game...");
-
+		
 	}
 
 
 }
-
-
-
-/*package project;
-import java.util.Random;
-import java.util.Scanner;
-
-
-public class DabloonGame extends Encounters {
-	private static Scanner scan = new Scanner(System.in);
-	protected int dabloonWallet;
-	private int event;
-
-	public DabloonGame(){
-		Traveller traveller = new Traveller();
-		System.out.println("\n########################################################################################"
-				+ "\nHello Traveller! I see you're new to this world.\nMay I know your name?.\nEnter your name: ");
-		String name = scan.nextLine();
-
-		Traveller.setName(name);
-		System.out.println("\n########################################################################################");
-		System.out.println("\nHello, " + Traveller.getName() + ".");
-		setDabloons(traveller.RandomDabloons(dabloonWallet));
-		dabloonWallet = getDabloons();
-		
-		encounters(dabloonWallet);
-		
-	}
-	
-	public void encounters(int dabloonWallet){
-
-
-		Boolean isLoop = true;
-
-
-		while(dabloonWallet < 2000 && isLoop == true) {
-			System.out.println("\n########################################################################################");
-			System.out.println("\nDo you wish to travel?");
-			System.out.println("\nYes / No");
-			System.out.println("Enter 'Inventory' to check inventory. ");
-			String ans = scan.nextLine();
-			String choice = ans.toLowerCase();
-
-			if(ans.equalsIgnoreCase("no")) {
-				isLoop = false;
-			}
-			else {
-
-				switch(choice) {
-				case "yes":{
-					Random rEvent = new Random(); 
-					event = rEvent.nextInt(1,3); 
-
-					switch(event){
-					case 1: {
-						dabloonWallet = task(dabloonWallet);
-						System.out.println("\nJust exited task here's how much you've earned: " + dabloonWallet);
-						continue;
-					}
-					case 2: {
-						dabloonWallet = merchant(dabloonWallet);
-						continue;
-
-					}
-					case 3: {//thieves
-
-						Random stolen = new Random(); 
-						int stolenDabloons = stolen.nextInt(50);
-						if(dabloonWallet > stolenDabloons) {
-
-							System.out.println("Oh no! There are thieves!!");
-							System.out.println("Dabloons is decreased by: " + stolenDabloons);
-							dabloonWallet -= stolenDabloons;
-							System.out.println("Dabloons decreased by " + stolenDabloons +" after encountering thieves.");
-							continue;
-						}
-
-					}
-					}
-
-				}
-				case "no":{
-					isLoop = false;
-				}
-				case "inventory" :{
-					Encounters inventory = new Encounters();
-					inventory.info();
-				}
-				default:{
-					break;
-				}
-				}
-
-			}
-		}
-
-	}
-	
-
-	public int getDabloons(){
-		return dabloonWallet;
-	}
-
-	public void setDabloons(int dabloonWallet) {
-		this.dabloonWallet = dabloonWallet;
-	}
-
-	public static void main(String[] args){
-		DabloonGame game = new DabloonGame();
-
-		System.out.println("Dabloons earned after playing: " + game.dabloonWallet);
-		System.out.println("Thank you for playing!");
-		System.out.println("Closing game...");
-		
-	}
-
-
-}*/
--- Encounters.java
+++ Encounters.java
@@ -1,36 +1,56 @@
 package project;
+
 import java.util.Scanner;
 import java.util.Random;
 
 
 
 
-public class Encounters extends Traveller{
-	private static Scanner scan = new Scanner(System.in);
+
+
+abstract class Encounters {
+	
+    private static Scanner scan = new Scanner(System.in);
 	protected int itemNum;
+	
+	public int answerParser (String answer) {
+		try {
+			  int num = Integer.parseInt(answer);
+			  return num;
+			} catch (NumberFormatException e) {
+			  System.out.println("Invalid number format");
+			  return 4;
+			}
+	}
+	
+	public int randomizer() {
+		Random task = new Random(); 
+		int taskNum = task.nextInt(6)+1;
+		return taskNum;
+	}
 
-
-
+	
 	public int task(int dabloonWallet){
 
 		Random task = new Random(); 
-		int taskNum = task.nextInt(1,6);
+		int taskNum = task.nextInt(1,6)+1;
 
 		if(taskNum == 1) {//task1
 			System.out.println("\n########################################################################################");
 			System.out.println("An elderly cat is asking for your assistance to carry his luggage...");
 			System.out.println("Help the elderly cat?");
-			System.out.println("Yes / No");
+			System.out.println("\n(Enter 1 to help, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nYou have accepted the offer!");
 				System.out.println("Elder Cat: Thank you for helping, here take this 70 dabloons.");
 				dabloonWallet += 70;
 				System.out.println("Dabloons after accepting offer: " + dabloonWallet);
 			}
 			else {
-				System.out.println("\nOffer rejected.");
+				System.out.println("\nOffer declined.");
 			}
 		}
 		else if (taskNum == 2) {//task2
@@ -38,17 +58,18 @@ public class Encounters extends Traveller{
 			System.out.println("Encountered a NPC cat!");
 			System.out.println("NPC cat is asking for your assistance to deliver letters to the village...");
 			System.out.println("Accept task?");
-			System.out.println("Yes / No");
+			System.out.println("\n(Enter 1 to accept, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nNPC Cat: Thanks for helping! ");
 				System.out.println("NPC Cat: I'll pay you 100 dabloons. ");
 				dabloonWallet += 100;
 				System.out.println("Dabloons after accepting offer: " + dabloonWallet);
 			}
 			else {
-				System.out.println("\nOffer rejected.");
+				System.out.println("\nOffer declined.");
 
 			}
 		}
@@ -56,79 +77,89 @@ public class Encounters extends Traveller{
 			System.out.println("\n########################################################################################");
 			System.out.println("Lost NPC is asking for directions!");
 			System.out.println("Help NPC?");
-			System.out.println("Yes / No");
+			System.out.println("\n(Enter 1 to help, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nYou gave directions for free.");
 				System.out.println("NPC: Thank you! Be careful on your journey...");
 			}
 			else {
-				System.out.println("\nOffer rejected.");
+				System.out.println("\nOffer declined.");
 			}
 		}
 		else if(taskNum == 4) {
 			System.out.println("\n########################################################################################");
 			System.out.println("NPC offering Hot Soup!");
 			System.out.println("Accept offer?");
-			System.out.println("Yes / No");
+			System.out.println("\n(Enter 1 to help, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nSoup accepted.");
 				System.out.println("NPC gave you 200 dabloons");
 				dabloonWallet += 200;
 			}
 			else {
-				System.out.println("\nSoup rejected.");
+				System.out.println("\nSoup declined.");
 			}
 		}
 		else if(taskNum == 5) {
 			System.out.println("\n########################################################################################");
 			System.out.println("NPC offering Bread!");
 			System.out.println("Accept offer?");
-			System.out.println("Yes / No");
+			System.out.println("\n(Enter 1 to accept, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nBread accepted.");
 			}
 			else {
-				System.out.println("\nBread rejected.");
-			}
-		}
-		else if(taskNum == 5) {
-			System.out.println("\n########################################################################################");
-			System.out.println("Inn Owner: Hey, it's getting quite cold outside.");
-			System.out.println("Inn Owner: Stay in my Inn?");
-			System.out.println("Staying in Inn costs 100 dabloons.");
-			System.out.println("\nYes / No");
-			String answer = scan.nextLine();
-
-			if(answer.equalsIgnoreCase("Yes")) {
-				System.out.println("\nStaying in Inn.");
-				dabloonWallet -= 100;
-				System.out.println("Dabloons after accepting offer: " + dabloonWallet);
-
-			}
-			else {
-				System.out.println("\nOffer rejected.");
+				System.out.println("\nBread declined.");
 			}
 		}
 		else if(taskNum == 6) {
 			System.out.println("\n########################################################################################");
+			System.out.println("Inn Owner: Hey, it's getting quite cold outside.");
+			System.out.println("Inn Owner: Stay in my Inn?");
+			System.out.println("Staying in Inn costs 100 dabloons.");
+			System.out.println("\n(Enter 1 to stay, any other key to decline)");
+			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
+
+			if(parsedAnswer == 1) {
+				if(dabloonWallet > 100) {
+				System.out.println("\nStaying in the Inn.");
+				dabloonWallet -= 100;
+				System.out.println("Dabloons after accepting offer: " + dabloonWallet);
+				}
+				else {
+					System.out.println("You don't have enough dabloons to stay in inn.");
+				}
+
+			}
+			else {
+				System.out.println("\nOffer declined.");
+			}
+		}
+		else if(taskNum == 7) {
+			System.out.println("\n########################################################################################");
 			System.out.println("You found an empty cabin!");
 			System.out.println("Enter cabin?");
-			System.out.println("\nYes / No");
+			System.out.println("\n(Enter 1 to enter, any other key to decline)");
 			String answer = scan.nextLine();
+			int parsedAnswer = answerParser(answer);
 
-			if(answer.equalsIgnoreCase("Yes")) {
+			if(parsedAnswer == 1) {
 				System.out.println("\nEntered cabin.");
-				System.out.println("You found 500 dabloons in the cabin!!");
+				System.out.println("Lucky! You found 500 dabloons in the cabin!");
 				dabloonWallet += 500;
 			}
 			else {
-				System.out.println("\nOffer rejected.");
+				System.out.println("\nOffer declined.");
 			}
 		}
 		return dabloonWallet;
@@ -136,65 +167,6 @@ public class Encounters extends Traveller{
 	}
 
 
-	public int merchant(int dabloonWallet, int itemNum) {
-
-		System.out.println("\n########################################################################################");
-		System.out.println("Encountered a shop along the way..");
-		Boolean buy = true;
-
-
-		while(buy == true) {
-			System.out.println("\nWELCOME TO ");
-			System.out.println("What item would you like to buy? We have a few...");
-			System.out.println("Items for sale: \nWater     --- 10 dabloons\nMap       --- 15 dabloons\nTorch     --- 40 dabloons\nBlanket   --- 80 dabloons\nTent      --- 200 dabloons  " );
-			System.out.println("\nWallet: " + dabloonWallet);
-			System.out.println("Enter any key to leave shop");
-			String answer = scan.nextLine();
-
-			if(answer.equalsIgnoreCase("water")) {
-				System.out.println("\nWater is added to inventory.");
-				dabloonWallet -= 10;
-
-				itemNum = 1;
-				return itemNum;
-			}
-			else if(answer.equalsIgnoreCase("map")){
-				System.out.println("\nMap is added to inventory.");
-				dabloonWallet -= 15;
-
-				itemNum = 2;
-				return itemNum;
-			}
-			else if(answer.equalsIgnoreCase("torch")){
-				System.out.println("\nTorch is added to inventory.");
-				dabloonWallet -= 40;
-
-				itemNum = 3;
-				return itemNum;
-			}
-			else if(answer.equalsIgnoreCase("blanket")){
-				System.out.println("\nBlanket is added to inventory.");
-				dabloonWallet -= 80;
-
-				itemNum = 4;
-				return itemNum;
-			}
-			else if(answer.equalsIgnoreCase("tent")){
-				System.out.println("\nTent is added to inventory");
-				dabloonWallet -= 200;
-
-				itemNum = 5;
-				return itemNum;
-
-			}
-			else {
-				System.out.println("You left the shop.");
-				buy = false;
-			}
-		}return dabloonWallet;
-	}
-
+	
 
 }
-
-
